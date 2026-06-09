9326a428945b52013b2a185d9a2efd01135f47e0
uy <a.normurzaev20@gmail.com>
2026-06-10 00:14:19 +0500

    Navbar qismi

--- loyiha-nomi/src/Campanents/Navbar/Navbar.jsx
+++ loyiha-nomi/src/Campanents/Navbar/Navbar.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom' // Sahifadan sahifaga o'tish uchun
 import logo from '../../assets/Logo (1).svg'
+import './Navbar.css' // CSS faylimiz
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="header">
-        <div className="container">
-            <div className="navbar-container">
-                <img src={logo} alt="" />
+    <div className="header">
+      <div className="container">
+        <div className="navbar-container">
+          {/* Logo qismi */}
+          <div className="logo-area">
+            <Link to="/">
+              <img src={logo} alt="Logo" />
+            </Link>
+          </div>
 
-                <ul>
-                    <li>Home</li>
-                    <li>Service</li>
-                    <li>Company</li>
-                    <li>Career</li>
-                    <li>Blog</li>
-                    <li>Contact us</li>
-                    <li>Clone project</li>
-                     
-                </ul>
-            </div>
+          {/* Menyular qismi */}
+          <ul className="nav-menu">
+            {/* O'sha sen aytgan, bosganda boshqa bo'limga o'tadigan 3 ta maxsus menyu */}
+            <li><Link to="/home" className="nav-link special-link">Home</Link></li>
+            <li><Link to="/service" className="nav-link special-link">Service</Link></li>
+            <li><Link to="/company" className="nav-link special-link">Company</Link></li>
+            
+            {/* Qolgan oddiy menyular */}
+            <li><Link to="/career" className="nav-link">Career</Link></li>
+            <li><Link to="/blog" className="nav-link">Blog</Link></li>
+            <li><Link to="/contact" className="nav-link">Contact us</Link></li>
+            <li><Link to="/clone" className="nav-link clone-btn">Clone project</Link></li>
+          </ul>
         </div>
       </div>
     </div>
