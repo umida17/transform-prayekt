269f14e45aa9ac5bd23aa182d8d5c8543c0e326e
uy <a.normurzaev20@gmail.com>
2026-06-10 00:37:08 +0500

    home qismi css

--- loyiha-nomi/src/Campanents/Layaut/Layaut.jsx
+++ loyiha-nomi/src/Campanents/Layaut/Layaut.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Home from '../../../public/Home/Home'
 
 const Layaut = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
