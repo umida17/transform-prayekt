625d300dc69acdf992aee1d77388dda092c77aa4
uy <a.normurzaev20@gmail.com>
2026-06-10 00:45:37 +0500

    home qismi css

--- loyiha-nomi/src/App.jsx
+++ loyiha-nomi/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Campanents/Navbar/Navbar'; 
-import Home from './Campanents/Home/Home';
+import Home from './Campanents/Home/Home'; // Home to'g'ri import qilingan
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<div>Asosiy sahifa kodi</div>} />
-        <Route path="/home" element={<div>Home sahifasi</div>} />
+        {/* Asosiy sahifada ham, /home sahifasida ham siz yaratgan chiroyli dizayn chiqadi */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        
+        {/* Qolgan sahifalar uchun vaqtinchalik div-lar */}
         <Route path="/service" element={<div>Service sahifasi</div>} />
         <Route path="/company" element={<div>Company sahifasi</div>} />
       </Routes>
