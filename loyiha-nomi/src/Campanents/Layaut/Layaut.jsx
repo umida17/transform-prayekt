1cc0801ee31a100549bac41f783ec540cdca9402
uy <a.normurzaev20@gmail.com>
2026-06-11 23:49:22 +0500

    Service

--- loyiha-nomi/src/Campanents/Layaut/Layaut.jsx
+++ loyiha-nomi/src/Campanents/Layaut/Layaut.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
  import Footer from '../Footer/Footer'
+ import Service from '../Service/Service'
 
 const Layaut = () => {
   return (
@@ -9,6 +10,7 @@ const Layaut = () => {
       <Navbar/>
       <Home/>
       <Footer/>
+      <Service/>
     </div>
   )
 }
